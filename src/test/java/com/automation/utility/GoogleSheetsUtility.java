f3561144b025c12eaf3693faf752c7478af83410
Abdul Ahad <bc230218248aah@vu.edu.pk>
2025-01-13 03:47:13 +0500

    is baar kuch bhi nahi, paka!

--- src/test/java/com/automation/utility/GoogleSheetsUtility.java
+++ src/test/java/com/automation/utility/GoogleSheetsUtility.java
@@ -20,7 +20,7 @@ public class GoogleSheetsUtility {
 
     private static final String APPLICATION_NAME = "Google Sheets Integration";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String CREDENTIALS_FILE_PATH = "credentials.json";
+    private static final String CREDENTIALS_FILE_PATH = "/Users/abdulahad/eclipse-workspace/seleniumAutomation/credentials.json";
 
     // Utility Method to Read and Increment Count from Google Sheets
     public  int getCountAndIncrement(String spreadsheetId, String range) {
